1956614227397a6fce09fc172d469adaeef77aaa
Daniel Buhagiar <daniel.buhagiar.g53167@mcast.edu.mt>
2025-02-27 15:55:48 +0000

    Added the Student and College controllers with the requested methods

---
+++ app/Http/Controllers/CollegeController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\College;
+use Illuminate\Http\Request;
+
+class CollegeController extends Controller
+{   
+    // Create
+
+    // Create a new college
+    public function create() {
+        $college = new College();
+        return view('colleges.create', compact('college'));
+    }
+
+    // Store the form data
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:colleges,name',
+            'address' => 'required'
+        ]);
+        
+        College::create($request->all());
+        return redirect()->route('home_page.index')->with('message', 'College has been saved successfully');
+    }
+
+    // Read
+
+    // Display all collages
+    public function index() {
+        $colleges = College::orderBy('name')->get();
+        return view('colleges.index', compact('colleges'));
+    }
+    
+    // Display college details
+    public function show($college_id) {
+        $college = College::find($college_id);
+        return view('colleges.show', compact('college'));
+    }
+
+    // Update
+
+    // Display the edit form
+    public function edit($college_id){
+        $college = College::find($college_id);
+        return view('colleges.edit', compact('college')); 
+    }
+
+    // Update the user details from the edit form
+    public function update($college_id, Request $request){
+        $request->validate([
+            'name' => 'required|unique:colleges,name,' . $college_id,  // exclude current college from unique check
+            'address' => 'required'
+        ]);        
+
+        $college = College::find($college_id);
+        $college->update($request->all());
+
+        return redirect()->route('home_page.index')->with('message', 'College has been updated successfully');
+    }
+
+    // Delete
+    
+    // Destroy the college with the id $id
+    public function destroy($college_id){
+        $college = College::find($college_id);
+        $college->delete();
+        return back()->with('message', 'college and comments have been deleted successfully');
+    }
+}
---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\College;
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+
+    // Create
+
+    // Create a new student
+    public function create() {
+        $student = new Student();
+        $colleges = College::orderBy('name')->pluck('name', 'id')->prepend('All Colleges', '');
+        return view('students.create', compact('colleges', 'student'));
+    }
+
+    // Store the form data
+    public function store(Request $request)
+    {
+        // Validate the incoming request
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:students,email',
+            'phone' => 'required|regex:/^\d{8}$/',  // Ensure phone has exactly 8 digits
+            'dob' => 'required|date|before:today', // Validate that the date of birth is a valid date and in the past
+            'college_id' => 'required|exists:colleges,id', // Ensure that college_id exists in the colleges table
+        ]);
+    
+        // Create the student
+        Student::create($request->all());
+    
+        // Redirect back to the posts index page with a success message
+        return redirect()->route('home_page.index')->with('message', 'Student has been saved successfully');
+    }
+
+    // Read
+
+    // Display all students
+    public function index() {
+        $colleges = College::orderBy('name')->pluck('name', 'id')->prepend('All Colleges', '');
+
+        if (request('college_id') == null) {
+            $students = Student::orderBy('name')->get();
+        } else {
+            $students = Student::where('college_id', request('college_id'))->orderBy('name')->get();
+        }
+        
+        return view('students.index', compact('students', 'colleges'));
+    }
+
+    // Display contact details
+    public function show($student_id) {
+        $student = Student::find($student_id);
+        return view('students.show', compact('student'));
+    }
+
+    // Update
+
+    // Display the edit form
+    public function edit($student_id){
+        $student = Student::find($student_id);
+        return view('students.edit', compact('student')); 
+    }
+
+    // Update the user details from the edit form
+    public function update($student_id, Request $request){
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:students,email'. $student_id, // Exclude the current students email from being unique
+            'phone' => 'required|regex:/^\d{8}$/',  // Ensure phone has exactly 8 digits
+            'dob' => 'required|date|before:today', // Validate that the date of birth is a valid date and in the past
+            'college_id' => 'required|exists:colleges,id', // Ensure that college_id exists in the colleges table
+        ]);
+
+        $student = Student::find($student_id);
+        $student->update($request->all());
+
+        return redirect()->route('home_page.index')->with('message', 'Student has been updated successfully');
+    }
+
+    // Delete
+
+    // Destroy the student with the id $id
+    public function destroy($student_id) {
+        $student = Student::findOrFail($student_id); // Ensure it throws an exception if not found
+        $student->delete();
+        return back()->with('message', 'Student has been deleted successfully');
+    }
+}
