27182e64cd8f98b02936c00d17b476268d83f6e4
Daniel Buhagiar <daniel.buhagiar.g53167@mcast.edu.mt>
2025-02-27 14:44:13 +0000

    Added Student and College Models for ORM

---
+++ app/Models/Student.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    // Enable mass assignment
+    protected $fillable = ["name", "email", "phone", "dob", "college_id"];
+
+    
+    // A student belongs to one college
+    public function college(){
+        return $this->belongsTo(College::class);
+    }
+}
