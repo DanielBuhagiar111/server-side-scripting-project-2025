1c91cd2290c2d80c8c567ad0c3870d4bd00a342c
Daniel Buhagiar <daniel.buhagiar.g53167@mcast.edu.mt>
2025-03-09 15:38:59 +0000

    Final Version of the code

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{csrf_token()}}">
 
-    <title>My Contact</title>
+    <title>Student App</title>
 
     <!-- Bootstrap -->
     <link href="{{ asset('/css/bootstrap.min.css')}}" rel="stylesheet">
